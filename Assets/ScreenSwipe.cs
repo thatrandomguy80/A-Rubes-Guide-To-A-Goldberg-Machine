b1a4a1d88c2406d5d43f61d557b94e098b1a95c4
MR Harrison Campbell <43701000@students.mq.edu.au>
2016-08-16 16:42:25 +1000

    Test Commit
    Added Swipe Visuals
    Added Trail for ball

---
+++ Assets/ScreenSwipe.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSwipe : MonoBehaviour {
+
+	private TrailRenderer trail;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update()
+	{
+		if(Input.GetMouseButton(0)){
+			transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			transform.position = new Vector3 (transform.position.x, transform.position.y-2, 5);
+		}
+	}
+
+}
