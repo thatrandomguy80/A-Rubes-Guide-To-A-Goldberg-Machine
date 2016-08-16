b8a73030b89ffe90feeabf595ccd87efe10e235c
MR Hayden Munday <43635199@students.mq.edu.au>
2016-08-16 16:52:24 +1000

    Added rotating script

---
+++ Assets/Resources/02_Scripts/RotatingObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatingObject : MonoBehaviour
+{
+    [Header("In degrees per tick")]
+    public float RotateSpeed = 2;//in degrees per tick
+    [Header("On is clockwise")]
+    public bool RotateDirection = true;
+
+    private int dir = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!RotateDirection)
+        {
+            dir = 1;
+        }
+        else
+        {
+            dir = -1;
+        }
+        this.transform.Rotate(new Vector3(0, 0, RotateSpeed * dir * Time.deltaTime));
+    }
+}
