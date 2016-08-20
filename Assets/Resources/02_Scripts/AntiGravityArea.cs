fb4dc0c013b95879972b1902a2b4989dda359bc3
Harrison Campbell <kolpiou@live.com>
2016-08-20 18:52:53 +1000

    AntiGravity Components Added
    Anti Gravity Area
    Objects that enter this area have their gravity flipped
    Objects that exit this area have their gravity returned to normal
    Anti Gravity Controller
    When the player presses the controller the gravity flips

---
+++ Assets/Resources/02_Scripts/AntiGravityArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntiGravityArea : MonoBehaviour {
+
+    //When an object enters the anti gravity area
+    //Their gravity will be inverted
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        FlipGravity(other.gameObject);
+    }
+    //When an object exits the anti gravity area
+    //Their gravity will return to normal
+    void OnTriggerExit2D(Collider2D other)
+    {
+        FlipGravity(other.gameObject);
+    }
+    //Flips the gravity of an object
+    private void FlipGravity(GameObject obj)
+    {
+        //If the object has a rigidbody
+        Rigidbody2D rigid2D = obj.GetComponent<Rigidbody2D>();
+        if (rigid2D != null)
+        {
+            //Flip the gravity
+            rigid2D.gravityScale = rigid2D.gravityScale*-1;
+        }
+    }
+    
+}
---
+++ Assets/Resources/02_Scripts/AntiGravityController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntiGravityController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    private void FlipGravityForAllObjects()
+    {
+        Rigidbody2D[] rigBods = FindObjectsOfType(typeof(Rigidbody2D)) as Rigidbody2D[];
+        for(int i = 0; i < rigBods.Length; i++)
+        {
+            rigBods[i].gravityScale = rigBods[i].gravityScale * -1;
+        }
+
+    }
+    void OnMouseDown()
+    {
+        FlipGravityForAllObjects();
+    }
+	
+}
