d8b7201f652177a04fb06fdd8f2f6b9157f8c34e
MR Hayden Munday <43635199@students.mq.edu.au>
2016-08-25 14:20:50 +1000

    InPersonSyncing

--- Assets/Resources/02_Scripts/Module_Based/Rope.cs
+++ Assets/Resources/02_Scripts/Module_Based/Rope.cs
@@ -14,19 +14,18 @@ public class Rope : MonoBehaviour {
         float length = tailPos.y - anchor.transform.position.y;
         if (length < 0) length = -length;//make pos
         float scale = 0.0166f * length;
-        this.transform.Translate(new Vector3(0, -(length/2), 0));//trashes
+        this.transform.Translate(new Vector3(0, -(length / 2), 0));//trashes
         this.transform.Rotate(new Vector3(0f, 0f, rot));
         this.transform.localScale = new Vector3(0.1f, scale, 0.1f);
-        if (anchor.GetComponent<Rigidbody2D>() == null) {//stationary anchors have rbs platform ones dont
-            Rigidbody2D rb = anchor.GetComponentInChildren<Rigidbody2D>();
-            headjoint = head.GetComponent<HingeJoint2D>();
-            if (rb != null && headjoint != null) {
-                headjoint.autoConfigureConnectedAnchor = false;
-                headjoint.connectedBody = rb;
-                headjoint.connectedAnchor = new Vector2(0, 0);
-            }
+        Rigidbody2D rb = anchor.GetComponentInChildren<Rigidbody2D>();
+        headjoint = head.GetComponent<HingeJoint2D>();
+        if (rb != null && headjoint != null) {
+            headjoint.autoConfigureConnectedAnchor = false;
+            headjoint.connectedBody = rb;
+            headjoint.connectedAnchor = new Vector2(0, 0);
         }
         //tail.transform.position = tailPos;
-        
+
+
     }
 }
