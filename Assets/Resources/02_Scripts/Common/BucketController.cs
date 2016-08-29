1da848eaeeec27dfe92515330b61d48241a0251f
Harrison Campbell <kolpiou@live.com>
2016-08-29 15:32:12 +1000

    Death Barrier Added, Bucket and background changed
    Death barrier is added to script and prefabs
    Bucket prefab created
    Background Changed

--- Assets/Resources/02_Scripts/Common/BucketController.cs
+++ Assets/Resources/02_Scripts/Common/BucketController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class BucketController : GameState {
 
+    //If the player collides with the bottom of the bucket
+    //They win and move to the next level
 	void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.transform.tag.Equals("Player"))
---
+++ Assets/Resources/02_Scripts/Common/DeathBarrier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathBarrier : GameState {
+
+    void Start()
+    {
+        //Hides the barrier
+        hideArea();
+    }
+    //Hides the defined area of play
+    private void hideArea()
+    {
+        MeshRenderer meshRen = GetComponent<MeshRenderer>();
+        meshRen.enabled = false;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //If the player exits the area they lose
+        if (other.transform.tag.Equals("Player"))
+        {
+            EndGame.Lose();
+        }
+    }
+}
