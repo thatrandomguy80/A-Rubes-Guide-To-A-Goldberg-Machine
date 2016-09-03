b78f5231c6141c5e8d2d23e7bf67d344c7b90436
thatrandomguy80 <thatrandomguy80@gmail.com>
2016-09-04 00:55:01 +1000

    Gave muilt cut within ropes an alive timer

--- Assets/Resources/02_Scripts/Module_Based/setRopeParent.cs
+++ Assets/Resources/02_Scripts/Module_Based/setRopeParent.cs
@@ -5,19 +5,26 @@ public class setRopeParent : MonoBehaviour {
     [Header("This sets the joints connected body at start")]
     public bool nothing;
 
+
+    private float aliveTime = 0.25f; // this sets when the multicut function will be aloud to be used after inst
+    private float timeAlive; // saving of time inst
     private HingeJoint2D joint;
+
+
     //This sets the joints connected body at start for the rope
     void Start () {
         joint = this.GetComponent<HingeJoint2D>();
         joint.connectedBody = this.transform.parent.GetComponent<Rigidbody2D>();
+        timeAlive = Time.time;
 	}
+
 
     public void enableD() {//called if the platform has a movement obj script attached to better limit the movement
         this.GetComponent<DistanceJoint2D>().enabled = true;
     }
 
     public void OnCollisionEnter2D(Collision2D other) {
-        if (other.collider.transform.name.Equals("Trail")) {
+        if (other.collider.transform.name.Equals("Trail") && Time.time - timeAlive > aliveTime) {//has it been alive for aliveTime amount of time.
             joint.connectedBody.useAutoMass = false;
             joint.connectedBody.mass = 20f;
             Destroy(joint);
