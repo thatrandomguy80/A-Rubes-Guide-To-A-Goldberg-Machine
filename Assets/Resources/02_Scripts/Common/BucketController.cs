8f3edbd03c7f38f5ee3dc50f3f089bd9cb0edc7d
MR Harrison Campbell <43701000@students.mq.edu.au>
2016-10-31 13:53:15 +1100

    Fixed Camera, Fixed Game Canvas, Fixed Bucket Win

--- Assets/Resources/02_Scripts/Common/BucketController.cs
+++ Assets/Resources/02_Scripts/Common/BucketController.cs
@@ -7,33 +7,47 @@ public class BucketController : GameState {
     //They win and move to the next level
 	void OnCollisionEnter2D(Collision2D coll)
     {
-		
-        if (coll.transform.tag.Equals("Player"))
-        {
-			Rigidbody2D rig2d = coll.transform.GetComponent<Rigidbody2D> ();
-			rig2d.constraints = RigidbodyConstraints2D.FreezeAll;
-			InGame.Stars.Add (); //They also get a star
-            ConfettiControllor cc = transform.parent.parent.GetChild(2).GetComponent<ConfettiControllor>();
-            if (cc != null) {
-                cc.Play();
-            }
-            EndGame.Win();
-
-        }
+		WinGameBucket(coll);
     }
     void OnTriggerEnter2D(Collider2D coll) {//trigger copy 
-
-        if (coll.transform.tag.Equals("Player")) {
-            Rigidbody2D rig2d = coll.transform.GetComponent<Rigidbody2D>();
-            rig2d.constraints = RigidbodyConstraints2D.FreezeAll;
-            InGame.Stars.Add(); //They also get a sta
-            ConfettiControllor cc = transform.parent.parent.GetChild(2).GetComponent<ConfettiControllor>();
-            if (cc != null) {
-                cc.Play();
-            }
-
-            EndGame.Win();
-
-        }
+		WinGameBucket(coll);    
     }
+
+	void WinGameBucket(Collision2D coll){
+		if (coll.transform.tag.Equals("Player")) {
+			Rigidbody2D rig2d = coll.transform.GetComponent<Rigidbody2D>();
+			rig2d.constraints = RigidbodyConstraints2D.FreezeAll;
+			InGame.Stars.Add(); //They also get a sta
+
+			coll.transform.parent = transform;
+
+
+			ConfettiControllor cc = transform.parent.parent.GetChild(2).GetComponent<ConfettiControllor>();
+			if (cc != null) {
+				cc.Play();
+			}
+
+			EndGame.Win();
+
+		}
+	}
+	void WinGameBucket(Collider2D coll){
+		if (coll.transform.tag.Equals("Player")) {
+			Rigidbody2D rig2d = coll.transform.GetComponent<Rigidbody2D>();
+			rig2d.constraints = RigidbodyConstraints2D.FreezeAll;
+			InGame.Stars.Add(); //They also get a sta
+
+			coll.transform.parent = transform;
+
+
+			ConfettiControllor cc = transform.parent.parent.GetChild(2).GetComponent<ConfettiControllor>();
+			if (cc != null) {
+				cc.Play();
+			}
+
+			EndGame.Win();
+
+		}
+	}
+
 }
